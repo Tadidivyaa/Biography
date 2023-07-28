9cea9bd82a7f4c97d8fbbaf7d65bfd62ebbe3826
Tadidivyaa <126139531+Tadidivyaa@users.noreply.github.com>
2023-07-28 20:54:40 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,22 @@
+ReactDOM.render(
+<div>
+<h1>This is a react page.</h1>
+<p>ReactJS is first introduced by "Jprdan Walke" and now it was maintained by"Facebook". </p>
+<ul>
+    <li>Yamaha</li>
+    <li>Bajaj</li>
+    <li>Honda</li>
+    <li>TVs</li>
+    <li>Keeway</li>
+</ul>
+<ol>
+    <li>BMW</li>
+    <li>Audi</li>
+    <li>Suzuki</li>
+    <li>ford</li>
+    
+</ol>
+<img src="https://www.1800flowers.com/blog/wp-content/uploads/2015/10/red-roses.jpg.webp" height="200px" width="300px"></img>
+</div>
+,
+document.getElementById("content"));
